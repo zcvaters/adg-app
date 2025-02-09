098f1147be1303ee9b34d825c27dc64ee0d89f88
Zach <zachvaters@gmail.com>
2025-02-09 15:42:50 -0330

    Add board members data and refactor board page with dynamic rendering

---
+++ src/data/board.ts
@@ -0,0 +1,44 @@
+export interface BoardMember {
+    name: string;
+    position: string;
+    imageUrl?: string;
+}
+
+export const boardMembers: BoardMember[] = [
+    {
+        name: "Ed Hartling",
+        position: "President",
+    },
+    {
+        name: "Adam Peddle",
+        position: "Vice President",
+    },
+    {
+        name: "Cordale Langford",
+        position: "Treasurer",
+    },
+    {
+        name: "Willem Peters",
+        position: "Secretary",
+    },
+    {
+        name: "Robert King",
+        position: "Member at Large",
+    },
+    {
+        name: "Frank Morin",
+        position: "Member at Large",
+    },
+    {
+        name: "Chris Evans",
+        position: "Member at Large",
+    },
+    {
+        name: "Zach Vaters",
+        position: "Member at Large",
+    },
+    {
+        name: "Keegan Pender",
+        position: "Junior Officer",
+    }
+]; 
